d8e9de49ec317b66318fd18a1258589ae5097d54
Matthew <mhoenig35@gmail.com>
2024-03-25 12:13:28 +0000

    got rid of comments

--- src/App.tsx
+++ src/App.tsx
@@ -16,31 +16,22 @@ import React from "react";
 
 
 function App() {
-      //set a useState for the user, so we can put in a prop to the Navbar
   const[user, setUser] = useState(null)
-//fetch the user info and the token for that user
   async function fetchUser(){
 
     const token = localStorage.getItem("token")
-    //the response is the getCurrentUser function and the token from the website
     const resp = await axios.get('/api/user', {
       headers: { Authorization: `Bearer ${token}` }
     })
     console.log(resp.data)
-    //the user has been set to the resp.data which is the user details (username, email, id)
     setUser(resp.data)
     
   }
-  //using useEffect to run this once when laoded in
   //if there is a token (so if user logged in) it will fetch the user
   useEffect(()=>{
     const token = localStorage.getItem("token")
     if(token) fetchUser()
   },[])
-
-
-
-
 
   return (
     <Router>
