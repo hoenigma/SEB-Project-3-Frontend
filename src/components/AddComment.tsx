d3fbff68b00fa844a6738afb65a110fdde5ec805
Matthew <mhoenig35@gmail.com>
2024-03-26 10:21:38 +0000

    started on comments page

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -1,7 +1,23 @@
+import React, { SyntheticEvent, useState } from "react"
+
+
 export default function Community () {
+
+    const [formData, setFormData] = useState({
+        title: "",
+        post: "",
+        date: "",
+        time: "",
+    })
+
+
 
     return <>
   <div>Hello World</div> 
+  <button className='button'> Add Post </button>
+  <button className='button'> Delete Post </button>
+  <button className='button'> Update Post </button>
+
     </>
 
 }
