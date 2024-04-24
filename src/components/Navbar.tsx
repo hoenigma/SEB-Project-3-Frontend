a8839752863d96c06ef8b3aa0c16b68c0dd50e95
ElTalbot <154763069+ElTalbot@users.noreply.github.com>
2024-04-24 17:38:21 +0100

    New repo front end

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { IUser } from "../interfaces/user";
 import logo from "../assets/logo.png";
 import "@fortawesome/fontawesome-free/css/all.css";
+import React from "react";
 
 interface NavbarProps {
   user: null | IUser;
@@ -12,6 +13,7 @@ function Navbar({ user, setUser }: NavbarProps) {
   console.log("user in the navbar:", user);
 
   const navigate = useNavigate();
+  const [isActive, setIsActive] = React.useState(false);
 
   function logout() {
     localStorage.removeItem("token");
@@ -19,14 +21,37 @@ function Navbar({ user, setUser }: NavbarProps) {
     navigate("/");
   }
 
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
   return (
-    <>
-      <header>
-        <nav className="navbar-is-transparent">
-          <div className="container">
-            <div className="navbar-menu">
+    <header>
+      <nav
+        className="navbar-is-transparent"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div
+            className={`navbar-menu ${isActive ? "is-active" : "is-active"}`}
+          >
+            <div className="navbar-brand">
+              <img className="image is-64x64" src={logo} alt="dog outline" />
+              <a
+                role="button"
+                className={`navbar-burger ${isActive ? "is-active" : ""}`}
+                aria-label="menu"
+                aria-expanded={isActive ? "true" : "false"}
+                onClick={toggleNavbar}
+                style={{ color: "rgb(127, 178, 121)" }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </a>
+            </div>
+            <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
               <div className="navbar-start">
-                <img className="image is-64x64" src={logo} alt="dog outline" />
                 <Link to="/" className="navbar-item has-text-light">
                   <span className="icon-text">
                     <span className="icon">
@@ -46,16 +71,15 @@ function Navbar({ user, setUser }: NavbarProps) {
               </div>
               <div className="navbar-end">
                 {!user && (
-                  <Link to="/login" className="navbar-item has-text-light">
-                    Login
-                  </Link>
+                  <>
+                    <Link to="/login" className="navbar-item has-text-light">
+                      Login
+                    </Link>
+                    <Link to="/signup" className="navbar-item has-text-light">
+                      Sign Up
+                    </Link>
+                  </>
                 )}
-                {!user && (
-                  <Link to="/signup" className="navbar-item has-text-light">
-                    Sign Up
-                  </Link>
-                )}
-                {/* Create a logout button, add a function logout to the onClick */}
                 {user && (
                   <button
                     onClick={logout}
@@ -67,10 +91,11 @@ function Navbar({ user, setUser }: NavbarProps) {
               </div>
             </div>
           </div>
-        </nav>
-      </header>
-    </>
+        </div>
+      </nav>
+    </header>
   );
 }
 
 export default Navbar;
+//
